fa0600f7ebb64b5221b29e8649d81cf47132d51e
DengrongGuan12 <1031212913@qq.com>
2016-03-13 23:04:05 +0800

    test

--- algorithm/test.c
+++ algorithm/test.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
-	int a, b;
-	scanf("%d%d",&a,&b);
-	int c = a+b;
-	printf("%d\n", c);
+	const double pi = 4.0 * atan(1.0);
+	double r,h,s1,s2,s;
+	scanf("%lf%lf",&r,&h);
+	s1 = pi*r*r;
+	s2 = 2*pi*r*h;
+	s = s1*2.0+s2;
+	printf("%.3lf\n", s);
 	return 0;
 }
